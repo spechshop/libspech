e805b4eb95c2fa633ab4c60d2cadac1199aabb67
lotus <lotus@spechshop.com>
2025-11-22 23:10:51 -0400

    Remove unused module references from configInterface.json

---
+++ stubs/bcg729Channel.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+
+class bcg729Channel {
+
+    
+    public function __construct() {
+        // stub
+    }
+
+    
+    public function decode(string $input): string {
+        return "";
+    }
+
+    
+    public function encode(string $input): string {
+        return "";
+    }
+
+    
+    public function info() {
+        // stub
+    }
+
+    
+    public function close() {
+        // stub
+    }
+}
